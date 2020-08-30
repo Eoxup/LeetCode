aa4b5727c9dd77710b55d99379cc7d070eb38862
Eoxup <bogusbook@gmail.com>
2020-08-31 00:11:00 +0800

    Problem 1545 performance enhancement
    Enhance bitwise execution algorithm
    Add description of this algorithm

--- Problems/1545. Find Kth Bit in Nth Binary String/Go/findKthBit.go
+++ Problems/1545. Find Kth Bit in Nth Binary String/Go/findKthBit.go
@@ -1,14 +1,23 @@
 func findKthBit(n int, k int) byte {
-    if k & 1 > 0 {
-        if k >> 1 & 1 == 0 {
-            return '0'
-        } else {
-            return '1'
-        }
+    /**********************************
+     binary k | ans | y | z |
+    ----------|-----|---|---|
+     xxxxxx01 | '0' | 0 | 0 |
+     xxxxxx11 | '1' | 1 | 0 |
+     x010...0 | '1' | 0 | 1 |
+     x110...0 | '0' | 1 | 1 |
+
+    #define  x = the first bit 1 of k from right side
+    #define  y = the bit left then x
+    #define  z = {
+        1, if k is even
+        0, if k is odd
     }
-    if k & ((k ^ (k - 1)) + 1) == 0 {
-        return '1'
+    ans = y ^ z
+    **********************************/
+    if (k & -k) << 1 & k > 0 {
+        return '0' + byte((k & 1) ^ 0)
     } else {
-        return '0'
+        return '0' + byte((k & 1) ^ 1)
     }
 }
