5be065db69e8e707bb1720c1e9d10e59e842470a
Eoxup <bogusbook@gmail.com>
2020-09-14 15:16:22 +0800

    Golang 1545. Unit test bug fix

--- Problems/1545. Find Kth Bit in Nth Binary String/Go/findKthBit_test.go
+++ Problems/1545. Find Kth Bit in Nth Binary String/Go/findKthBit_test.go
@@ -13,12 +13,13 @@ func TestFindKthBit(t *testing.T) {
 		{3, 1, '0'},
 		{4, 11, '1'},
 		{1, 1, '0'},
-		{2, 3, '0'},
+		{2, 3, '1'},
 	}
 
 	for _, tc := range testCases {
+		tc := tc
 		testname := fmt.Sprintf("FindKthBit(%d,%d)", tc.n, tc.k)
-		t.Run(testname, func(tt *testing.T){
+		t.Run(testname, func(t *testing.T) {
 			r := FindKthBit(tc.n, tc.k)
 			if r != tc.out {
 				t.Errorf("'%c' = FindKthBit(%d, %d). Want '%c'", r, tc.n, tc.k, tc.out)
